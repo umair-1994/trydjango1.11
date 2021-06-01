542b02c6331c30ae56f5e2204e619cd5d917605e
Umair Cheema <umaircheema1994@gmail.com>
2021-06-01 12:23:43 +0500

    First hello world program

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from restaurants.views import home
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', home),
 ]
